9ee4b4fa9ca57189adf33ed74094ed6804022b8e
tangchunling <1096326855@qq.com>
2016-03-22 18:14:10 +0800

    update

--- pc/js/head.js
+++ pc/js/head.js
@@ -1,8 +1,20 @@
 $(".navList ul li").hover(function(){
+	if(!$(this).is(".navlist-active")){
 	$(this).find("img").removeClass("nav-img-show");
-	$(this).find("img:last-child").addClass("nav-img-show");
+	$(this).find("img:last-child").addClass("nav-img-show");}
 })
 $(".navList ul li").mouseleave(function(){
-	$(this).find("img").removeClass("nav-img-show");
-	$(this).find("img:first-child").addClass("nav-img-show");
+	if(!$(this).is(".navlist-active")){
+		$(this).find("img").removeClass("nav-img-show");
+		$(this).find("img:first-child").addClass("nav-img-show");
+	}
+	
 })
+$(".navList ul li").click(function(){
+			$(".navList ul li").removeClass("navlist-active");
+			$(this).addClass("navlist-active");
+			$(".navList ul li").find("img").removeClass("nav-img-show");
+			$(".navList ul li").find("img:first-child").addClass("nav-img-show");
+			$(this).find("img").removeClass("nav-img-show");
+			$(this).find("img:last-child").addClass("nav-img-show");
+	});
